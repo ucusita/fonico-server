763e9c12dbf4836d0c0d0f142f43e08b0222dbd5
Marcelo R <marcelofabio01@yahoo.com.ar>
2021-08-15 12:59:41 -0300

    Implement secrets

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,12 +1,17 @@
 require("dotenv").config({
-  path: process.env.NODE_ENV === "development" ? ".env.development" : ""
+  path: process.env.NODE_ENV === "development" ? ".env.development" : ".env"
 });
 
 const config = {
   development: {
     mongoURL: process.env.DATABASE_URL,
     port: process.env.PORT
+  },
+  production: {
+    mongoURL: process.env.DATABASE_URL,
+    port: process.env.PORT
   }
+
 };
 
 module.exports = config[process.env.NODE_ENV];
